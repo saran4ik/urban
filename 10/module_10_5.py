8e3c62743b89016a63f406733061e5947fd747ff
Ярик <saran4ik@yandex.ru>
2025-01-14 10:27:56 +0500

    11
    Signed-off-by: Ярик <saran4ik@yandex.ru>

--- 10/module_10_5.py
+++ 10/module_10_5.py
@@ -1,17 +1,17 @@
 import multiprocessing
 from datetime import datetime as dt
 
-def read_info(name):
+def read_info(filename):
     all_data = []
-    with open(name, 'r', encoding='utf8') as file:
+    with open(filename, 'r', encoding='utf8') as file:
         for line in file:
             all_data.append(line)
 
-files = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
 start_l = dt.now()
 
-for file in files:
-    read_info(file)
+for filename in filenames:
+    read_info(filename)
 end_l = dt.now()
 time_line = end_l - start_l
 print(f'Время работы линейного вызова : {time_line}')
@@ -19,7 +19,7 @@ print(f'Время работы линейного вызова : {time_line}')
 if __name__ == '__main__':
     start_m = dt.now()
     with multiprocessing.Pool(processes=4) as pool:
-        pool.map(read_info, files)
+        pool.map(read_info, filenames)
     end_m = dt.now()
     time_multi = end_m - start_m
     print(f'Время работы мультипроцесса : {time_multi}')
